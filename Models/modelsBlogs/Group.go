813c513ef65890345433d499f4cf18cb8bbab91a
Jimmy <jimmyrss1102@gmail.com>
2023-04-15 06:50:38 +0800

    feat : add `create_at` and `update_at`

--- Models/modelsBlogs/Group.go
+++ Models/modelsBlogs/Group.go
@@ -1,7 +1,11 @@
 package modelsBlogs
 
+import "time"
+
 type BlogGroups struct {
-	ID     uint   `gorm:"primary_key"`
-	Group  string `gorm:"group" `
-	IsShow bool   `gorm:"is_show" `
+	ID       uint      `gorm:"primary_key"`
+	Group    string    `gorm:"group"`
+	IsShow   bool      `gorm:"is_show"`
+	CreateAt time.Time `gorm:"column:create_at"`
+	UpdateAt time.Time `gorm:"column:update_at"`
 }
--- Services/blogs/servicesAdmin/group.go
+++ Services/blogs/servicesAdmin/group.go
@@ -4,14 +4,19 @@ import (
 	Init "blog-go/Config"
 	"blog-go/Models/modelsBlogs"
 	"log"
+	"time"
 )
 
 // CreateGroup 创建分组
 func CreateGroup(group string) int64 {
+	now := time.Now()
 	var newGroup = modelsBlogs.BlogGroups{
-		Group:  group,
-		IsShow: true,
+		Group:    group,
+		IsShow:   true,
+		CreateAt: now,
+		UpdateAt: now,
 	}
+
 	result := Init.DB.Create(&newGroup)
 	println(result)
 	if result.Error != nil {
@@ -22,7 +27,8 @@ func CreateGroup(group string) int64 {
 
 // DeleteGroup 删除分组
 func DeleteGroup(id uint) int64 {
-	result := Init.DB.Table("blog_groups").Where("id=?", id).Update("is_show", false)
+	now := time.Now()
+	result := Init.DB.Table("blog_groups").Where("id=?", id).Updates(modelsBlogs.BlogGroups{IsShow: false, UpdateAt: now})
 	println(result)
 	if result.Error != nil {
 		log.Println("DeleteGroup group fail : ", result)
@@ -32,7 +38,8 @@ func DeleteGroup(id uint) int64 {
 
 // UpdateGroup 分组更新
 func UpdateGroup(id uint, group string) int64 {
-	result := Init.DB.Table("blog_groups").Where("id=?", id).Update("group", group)
+	now := time.Now()
+	result := Init.DB.Table("blog_groups").Where("id=?", id).Updates(modelsBlogs.BlogGroups{Group: group, UpdateAt: now})
 	println(result)
 	if result.Error != nil {
 		log.Println("UpdateGroup group fail : ", result)
@@ -46,7 +53,7 @@ func SelectGroup(paging int, pageSize int) []modelsBlogs.BlogGroups {
 	var blogGroups []modelsBlogs.BlogGroups
 	err := Init.DB.Table("blog_groups").Limit(pageSize).Offset((paging - 1) * pageSize).Find(&blogGroups)
 	if err.Error != nil {
-		log.Println("SelectGroup group fail : ", err)
+		log.Println("SelectContent group fail : ", err)
 	}
 	return blogGroups
 }
