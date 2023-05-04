f10d609fa6188fc88d329d127052f328066c107b
Jimmy <jimmyrss1102@gmail.com>
2023-05-05 02:15:30 +0800

    refactor(blog): Compose Update Set ` UpdateBlog`

--- Controllers/blogs/controlAdmin/blog.go
+++ Controllers/blogs/controlAdmin/blog.go
@@ -1,17 +1,19 @@
 package controlAdmin
 
 import (
-	"blog-go/Models/modelsBlogs"
+	"blog-go/Models/modelAdmin"
+	"blog-go/Models/modelPublic"
 	"blog-go/Services/blogs/servicesAdmin"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateBlog(context *gin.Context) {
-	var data modelsBlogs.BlogBlogs
+	var data modelAdmin.BlogBlogs
 	err := context.ShouldBindJSON(&data)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
 			"message": err.Error(),
 		})
 		return
@@ -32,133 +34,51 @@ func CreateBlog(context *gin.Context) {
 }
 
 func UpdateBlog(context *gin.Context) {
-	var data modelsBlogs.BlogBlogs
+	var data modelAdmin.BlogBlogs
 	err := context.ShouldBindJSON(&data)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
+		return
+	}
+	println("error,message", data.GroupId)
+	res := servicesAdmin.UpdateBlog(data.ID, data.GroupId, data.Title, data.Content, data.IsShow)
+	if res > 0 {
+		context.JSON(http.StatusOK, gin.H{
+			"code":    200,
+			"message": "blog更新成功",
+		})
+	} else {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
+			"message": "blog更新失败",
+		})
+	}
+}
+
+func SelectBlog(context *gin.Context) {
+	var page modelPublic.Page
+	err := context.ShouldBindJSON(&page)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{
 			"message": err.Error(),
 		})
 		return
 	}
-	println("error,message", data.GroupId)
-	var message string
-	error := 0
-	if data.GroupId != 0 {
-		res := servicesAdmin.UpdateBlogGroupId(data.ID, data.GroupId)
-		if res > 0 {
-			message = message + "blog分组更新成功。"
-		} else {
-			message = message + "blog分组更新失败。"
-			error += 1
-		}
-		println(error, message)
-	}
-	if data.Title != "" {
-		res := servicesAdmin.UpdateBlogTitle(data.ID, data.Title)
-		if res > 0 {
-			message = message + "blog标题更新成功"
-		} else {
-			message = message + "blog标题更新失败"
-			error += 1
-		}
-		println(error, message)
-	}
-	if data.Content != "" {
-		res := servicesAdmin.UpdateBlogData(data.ID, data.Content)
-		if res > 0 {
-			message = message + "blog内容更新成功"
-		} else {
-			message = message + "blog内容更新失败"
-			error += 1
-		}
-		println(error, message)
-	}
-	//查询是否相等
-	resIsShow := servicesAdmin.GetBlogIsShow(data.ID)
-	if data.IsShow != resIsShow {
-		if data.IsShow {
-			res := servicesAdmin.UpdateBlogIsShow(data.ID, data.IsShow)
-			if res > 0 {
-				message = message + "blog展示成功"
-			} else {
-				message = message + "blog展示失败"
-				error += 1
-			}
-		} else {
-			res := servicesAdmin.UpdateBlogIsShow(data.ID, data.IsShow)
-			if res > 0 {
-				message = message + "blog取消展示成功"
-			} else {
-				message = message + "blog取消展示失败"
-				error += 1
-			}
-		}
-	} else {
-		message = message + "blog展示状态相等"
-	}
-
-	if error > 0 {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"code":    400,
-			"message": message,
-		})
-	} else {
+	println(page.Paging, page.PageSize)
+	res := servicesAdmin.SelectBlog(page.Paging, page.PageSize)
+	if len(res) > 0 {
 		context.JSON(http.StatusOK, gin.H{
 			"code":    200,
-			"message": message,
+			"message": "查询成功",
+			"date":    res,
+		})
+	} else {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"code":    400,
+			"message": "查询失败",
 		})
 	}
 }
-
-//func ContentIsShow(context *gin.Context) {
-//	var data modelsBlogs.BlogContents
-//	err := context.ShouldBindJSON(&data)
-//	if err != nil {
-//		context.JSON(http.StatusBadRequest, gin.H{
-//			"message": err.Error(),
-//		})
-//		return
-//	}
-//	res := servicesAdmin.ContentIsShow(data.ID, data.IsShow)
-//	if res > 0 {
-//		context.JSON(http.StatusOK, gin.H{
-//			"code":    200,
-//			"message": "内容成功",
-//		})
-//	} else {
-//		context.JSON(http.StatusBadRequest, gin.H{
-//			"code":    400,
-//			"message": "隐藏内容失败",
-//		})
-//	}
-//}
-
-//type Page struct {
-//	Paging   int `json:"paging"`
-//	PageSize int `json:"pageSize"`
-//}
-
-//func SelectContent(context *gin.Context) {
-//	var page Page
-//	err := context.ShouldBindJSON(&page)
-//	if err != nil {
-//		context.JSON(http.StatusBadRequest, gin.H{
-//			"message": err.Error(),
-//		})
-//		return
-//	}
-//	println(page.Paging, page.PageSize)
-//	res := servicesAdmin.SelectGroup(page.Paging, page.PageSize)
-//	if len(res) > 0 {
-//		context.JSON(http.StatusOK, gin.H{
-//			"code":    200,
-//			"message": "查询成功",
-//			"date":    res,
-//		})
-//	} else {
-//		context.JSON(http.StatusBadRequest, gin.H{
-//			"code":    400,
-//			"message": "查询失败",
-//		})
-//	}
-//}
--- Services/blogs/servicesAdmin/blog.go
+++ Services/blogs/servicesAdmin/blog.go
@@ -2,14 +2,14 @@ package servicesAdmin
 
 import (
 	Init "blog-go/Config"
-	"blog-go/Models/modelsBlogs"
+	"blog-go/Models/modelAdmin"
 	"log"
 	"time"
 )
 
 // CreateBlog 创建内容
 func CreateBlog(groupId int, title, content string) int64 {
-	var newGroup = modelsBlogs.BlogBlogs{
+	var newGroup = modelAdmin.BlogBlogs{
 		GroupId:  groupId,
 		Title:    title,
 		Content:  content,
@@ -26,67 +26,39 @@ func CreateBlog(groupId int, title, content string) int64 {
 	return result.RowsAffected
 }
 
-// GetBlogIsShow  查询blog是否展示
-func GetBlogIsShow(id uint) bool {
-	type BlogContent struct {
-		IsShow bool
-	}
-	var blogContent BlogContent
-	result := Init.DB.Table("blog_blogs").Select("is_show").Where("id= ?", id).Scan(&blogContent)
-	println(result)
-	if result.Error != nil {
-		log.Println("ContentIsShow group fail : ", result)
-	}
-	return blogContent.IsShow
-}
-
-// UpdateBlogGroupId  blog分组更新
-func UpdateBlogGroupId(id uint, groupId int) int64 {
-	result := Init.DB.Table("blog_blogs").Where("id=?", id).Updates(&modelsBlogs.BlogBlogs{GroupId: groupId, UpdateAt: time.Now()})
-	println(result)
-	if result.Error != nil {
-		log.Println("group_id update fail : ", result)
-	}
-	return result.RowsAffected
-}
-
-// UpdateBlogTitle  blog标题分组
-func UpdateBlogTitle(id uint, title string) int64 {
-	result := Init.DB.Table("blog_blogs").Where("id=?", id).Updates(&modelsBlogs.BlogBlogs{Title: title, UpdateAt: time.Now()})
-	println(result)
-	if result.Error != nil {
-		log.Println("group_id update fail : ", result)
-	}
-	return result.RowsAffected
-}
-
-// UpdateBlogData  blog内容更新
-func UpdateBlogData(id uint, content string) int64 {
-	result := Init.DB.Table("blog_blogs").Where("id=?", id).Updates(&modelsBlogs.BlogBlogs{Content: content, UpdateAt: time.Now()})
-	println(result)
-	if result.Error != nil {
-		log.Println("content update fail : ", result)
-	}
-	return result.RowsAffected
-}
-
-// UpdateBlogIsShow  blog是否展示
-func UpdateBlogIsShow(id uint, isShow bool) int64 {
-	result := Init.DB.Table("blog_blogs").Where("id=?", id).Updates(&modelsBlogs.BlogBlogs{IsShow: isShow, UpdateAt: time.Now()})
-	println(result)
-	if result.Error != nil {
-		log.Println("is_show update fail : ", result)
-	}
-	return result.RowsAffected
-}
-
-// SelectContent 分组查询
-// 第几页
-//func SelectContent(paging int, pageSize int) []modelsBlogs.BlogContents {
-//	var blogGroups []modelsBlogs.BlogContents
-//	err := Init.DB.Table("blog_content").Limit(pageSize).Offset((paging - 1) * pageSize).Find(&blogGroups)
-//	if err.Error != nil {
-//		log.Println("SelectContent group fail : ", err)
+//// GetBlogIsShow  查询blog是否展示
+//func GetBlogIsShow(id uint) bool {
+//	type BlogContent struct {
+//		IsShow bool
 //	}
-//	return blogGroups
+//	var blogContent BlogContent
+//	result := Init.DB.Table("blog_blogs").Select("is_show").Where("id= ?", id).Scan(&blogContent)
+//	println(result)
+//	if result.Error != nil {
+//		log.Println("ContentIsShow group fail : ", result)
+//	}
+//	return blogContent.IsShow
 //}
+
+// UpdateBlog blog更新
+func UpdateBlog(id uint, groupId int, title string, content string, isShow bool) int64 {
+	result := Init.DB.Table("blog_blogs").
+		Where("id=?", id).
+		Updates(&modelAdmin.BlogBlogs{GroupId: groupId, Title: title, Content: content, IsShow: isShow, UpdateAt: time.Now()})
+	println(result)
+	if result.Error != nil {
+		log.Println("blog_blogs update fail : ", result)
+	}
+	return result.RowsAffected
+}
+
+// SelectBlog blog查询
+// 第几页
+func SelectBlog(paging int, pageSize int) []modelAdmin.BlogGroups {
+	var blogGroups []modelAdmin.BlogGroups
+	err := Init.DB.Table("blog_blogs").Limit(pageSize).Offset((paging - 1) * pageSize).Find(&blogGroups)
+	if err.Error != nil {
+		log.Println("SelectContent group fail : ", err)
+	}
+	return blogGroups
+}
